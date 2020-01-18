2cc6391f71fa3b545951bf81b7d3341a72023d2c
Player 1 <j.banuelos00@gmail.com>
2020-01-18 01:45:29 -0800

    Added in functions to show slider values, and copy button functionality

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ function generatePass(){
     //write the password result to the textbox
     document.getElementById("password").value = pass;
 }
+
+//display length display
+document.getElementById("length").innerHTML = "Length:64";
+
+//function to set length text based on the slider value
+document.getElementById("slider").oninput = function() {
+    if(document.getElementById("slider").value > 0) {
+        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+
+    } else{
+        document.getElementById("length").innerHTML = "Length: 8";
+    }
+}
+
+//copying password to clipboard
+function copyPasta() {
+    document.getElementById("password").select();
+
+    document.execCommand("copy");
+
+    alert("Password has been copied to your clipboard.");
+
+}
